68195e6e91f1677c74201af505f59be0d20b876a
Joke Heyndels <jo_hannah@outlook.com>
2022-10-14 16:03:31 +0000

    DISABLE_COLLECTSTATIC in settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -35,7 +35,8 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-
+if "DEV" not in os.environ:
+    DISABLE_COLLECTSTATIC = 1
 
 # Application definition
 
